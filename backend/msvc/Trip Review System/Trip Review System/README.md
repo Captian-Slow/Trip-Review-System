493575057548c6bc400b968f71ebae212fa3a2fe
Amit Kumar Gupta <theamit97@gmail.com>
2018-11-17 17:37:07 +0530

    organising for cross platform

---
+++ backend/msvc/Trip Review System/Trip Review System/README.md
@@ -0,0 +1,3 @@
+﻿# Trip Review System
+
+
---
+++ backend/msvc/Trip Review System/Trip Review System/src/middleware/logger.js
@@ -0,0 +1,5 @@
+﻿exports.log_time_date_req = function log_time_date_req(req, res, next) {
+    console.log('NEW_REQ: ', new Date(), req.method, req.url);
+    next();
+}
+
